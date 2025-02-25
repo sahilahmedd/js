363b0688c57a70576deb7b0e4a20d81ebb2daad9
sahiltcg <sahilahmedtcg@gmail.com>
2025-02-25 11:59:30 +0530

    Data type conversion

---
+++ 03_typeConversion.js
@@ -0,0 +1,29 @@
+// Type conversion
+
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+
+
+
+
+let isLoggedin = "Sahil"
+
+// let booleanIsLoggedIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Sahil" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
