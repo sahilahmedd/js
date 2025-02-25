79201be6305e6655e0bdaa92f993ebdc231db6a9
sahiltcg <sahilahmedtcg@gmail.com>
2025-02-25 11:33:18 +0530

    JS updated

---
+++ 02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // use newer version of JavaScript
+
+console.log(3
+    +
+    3); // code readability should be high: this example is not the correct way to write the code
+
+
+console.log("Sahil");
+
+/* 
+Number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symnbol => unique identifier
+*/
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); //undefined
+
+
+
+
+
