1a3538f288b4e94553352e66d8d43ceca4600318
sahiltcg <sahilahmedtcg@gmail.com>
2025-02-25 11:01:58 +0530

    JS

---
+++ 1.js
@@ -0,0 +1,53 @@
+// let me = 4;
+// let i = Number("Geek");
+// let you = String(true);
+// let has = null;
+// let nothing;
+
+// console.log(typeof me);
+// console.log(typeof i);
+// console.log(typeof you);
+// console.log(typeof has);
+// console.log(typeof nothing);
+
+
+// Conditions Switch
+
+// let ch = 6;
+
+// switch(ch) {
+//     case 1: 
+//         console.log("Ben Ten");
+//         break;
+//     case 2: 
+//         console.log("Doremon");
+//         break;
+//     case 3: 
+//         console.log("Kitreksu");
+//         break;
+//     default: 
+//         console.log("I don't watch shit!!!");
+// }
+
+// Loops 
+
+// for(let i = 0; i<=20;i++){
+//     console.log(i);
+// }
+
+// Functions 
+
+// function me(a,b,c) {
+//     return a+b+c;
+// }
+
+// console.log(me(12, 34, 46));
+// console.log(me(10, 54, 46));
+// console.log(me(16, 35, 65));
+// console.log(me(62, 89, 43));
+// console.log(me(42, 94, 57));
+
+
+// Arrays
+const myArr = [0,1,2,3,4,5]
+console.log(myArr[4]);
